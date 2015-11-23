fa9d5074762bb32766a549faa5a43b99de54bbbc
J. Francisco Raposerias <rapofran@gmail.com>
2015-11-23 02:26:24 -0300

    New sounds. Playin with 3 devices :)

--- index.html.bash
+++ index.html.bash
@@ -6,41 +6,22 @@
     </head>
     <body>
         <script>
-         var Url = function() {
-             var l = window.location;
+            var parts = window.location.href.split('/');
+            var id = parts[parts.length - 1];
 
-             var params = {};
+            var ws = new WebSocket('ws://{{IP}}:8080');
 
-             l.search
-              .substr(1)
-              .split('&')
-              .map(function(e){
-                  return e.split('=');
-              })
-              .forEach(function(kv){
-                  var k = kv[0];
-                  var v = kv[1];
-                  params[decodeURIComponent(k)] = decodeURIComponent(v);
-              })
+            function deviceMotionHandler(e) {
+               accel = {id: id, x: e.accelerationIncludingGravity.x, y: e.accelerationIncludingGravity.y, z: e.accelerationIncludingGravity.z }
+               ws.send(JSON.stringify(accel));
+            }
 
-             this.params = params;
-         }
-
-         var url = new Url();
-         var id = url.params.id;
-         var ws = new WebSocket('ws://{{IP}}:8080');
-
-         function deviceMotionHandler(e) {
-             accel = {id: id, x: e.accelerationIncludingGravity.x, y: e.accelerationIncludingGravity.y, z: e.accelerationIncludingGravity.z }
-             ws.send(JSON.stringify(accel));
-         }
-
-         if (window.DeviceMotionEvent) {
-             console.log('orientation and motion supported');
-             window.addEventListener('devicemotion', deviceMotionHandler);
-         } else {
-             alert('Esto no funciona en este browser')
-         }
+            if (window.DeviceMotionEvent) {
+               console.log('orientation and motion supported');
+               window.addEventListener('devicemotion', deviceMotionHandler);
+            } else {
+               alert('Esto no funciona en este browser')
+            }
         </script>
     </body>
 </html>
